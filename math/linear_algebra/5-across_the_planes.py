f572a49cd5ad36249d0fedf133aab936fecef5d3
enxo7899 <enxo7899@gmail.com>
2023-02-25 10:36:15 -0800

    task5.1

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add_matrices2D(mat1, mat2):
+    mat = []
+    if len(mat1[0]) != len(mat2[0]) or len(mat1) != len(mat2):
+        return None
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        mat.append(row)
+    return mat
+
+                                                                    
