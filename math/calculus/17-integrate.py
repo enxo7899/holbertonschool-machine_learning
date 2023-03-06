f09d031f4ee2991c041a0088a733c05ee12684fe
enxo7899 <enxo7899@gmail.com>
2023-03-06 09:02:47 -0800

    task 0

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-def poly_derivative(poly):
-    if type(poly) is not list or poly == []:
-        return None
-    elif len(poly) < 2:
-        return[0]
-    else:
-        derivative = poly.copy()
-        exponent = 1
-        derivative.pop(0)
-        for i in range(len(derivative)):
-            derivative[i] = derivative [i] * exponent
+def poly_integral(poly, C=0):
+    if type poly is not list or len(poly)==0:
+        return
+    elif type (c) is int:
+    if poly == [0]:
+        return C
+    exponent = 0
+    integral = poly.copy()
+    for i in range (len(integral)):
+        if type(integral[i]) is int or type (integral[i]) is float:
             exponent += 1
-        return derivative 
+            number = integral[i] / exponent
+            integral[i] = int(number) if number % 1 == 0 else number
+        else:
+            return None
+        integral.insert(0,c)
+        return integral 
