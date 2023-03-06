309675a7386d0d37e69116758aa33b44d74400b8
enxo7899 <enxo7899@gmail.com>
2023-03-06 03:32:48 -0800

    task 121

---
+++ math/linear_algebra/12-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+from typing import Tuple
+def np_elementwise(mat1: np.ndarray, mat2: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    add = np.add(mat1, mat2)
+    sub = np.subtract(mat1, mat2)
+    mul = np.multiply(mat1, mat2)
+    div = np.divide(mat1, mat2)
+    return (add, sub, mul, div)
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+print(mat1)
+print(mat2)
+add, sub, mul, div = np_elementwise(mat1, mat2)
+print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
+add, sub, mul, div = np_elementwise(mat1, 2)
+print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
