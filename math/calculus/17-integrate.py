2712e42189e28f13c87e82a99ebfb3f3682fa056
enxo7899 <enxo7899@gmail.com>
2023-03-07 04:11:29 -0800

    task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -5,14 +5,14 @@ def poly_integral(poly, C=0):
     elif type(C) is int:
         if poly == [0]:
             return C
-    exponent = 0
-    integral = poly.copy()
-    for i in range (len(integral)):
-        if type(integral[i]) is int or type (integral[i]) is float:
-            exponent += 1
-            number = integral[i] / exponent
-            integral[i] = int(number) if number % 1 == 0 else number
-        else:
-            return None
+        exponent = 0
+        integral = poly.copy()
+        for i in range (len(integral)):
+            if type(integral[i]) is int or type (integral[i]) is float:
+                exponent += 1
+                number = integral[i] / exponent
+                integral[i] = int(number) if number % 1 == 0 else number
+            else:
+                return None
         integral.insert(0,C)
         return integral 
