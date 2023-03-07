caa749a67e01593f8e819e5e3d69fa776f4ae755
enxo7899 <enxo7899@gmail.com>
2023-03-07 04:01:59 -0800

    task 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+""" Derive happines in oneself from a good day's work"""
+
+
 def poly_derivative(poly):
+    """function calculates the derivvative of the polynomun"""
     if type(poly) is not list or poly == []:
         return None
     elif len(poly) < 2:
@@ -9,6 +13,6 @@ def poly_derivative(poly):
         exponent = 1
         derivative.pop(0)
         for i in range(len(derivative)):
-            derivative[i] = derivative [i] * exponent
+            derivative[i] = derivative[i] * exponent
             exponent += 1
-        return derivative 
+        return derivative
