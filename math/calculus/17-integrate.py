73a137c2a96442817bc171d177db116fcbe2c061
enxo7899 <enxo7899@gmail.com>
2023-03-07 04:04:07 -0800

    task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -2,7 +2,7 @@
 def poly_integral(poly, C=0):
     if type poly is not list or len(poly)==0:
         return
-    elif type (c) is int:
+    elif type(c) is int:
     if poly == [0]:
         return C
     exponent = 0
