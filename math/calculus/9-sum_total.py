f0855ac0fa37d4c4f00d07a4a2dc7fd0d51481b5
enxo7899 <enxo7899@gmail.com>
2023-03-07 03:54:39 -0800

    task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+""" A function that calculates the sums of squares"""
+
+
 def summation_i_squared(n):
+    """ Summation of squared numbers"""
     if type(n) is not int or n<1:
         return None
     return int(n * (n+1) * (2*n+1) / 6)
