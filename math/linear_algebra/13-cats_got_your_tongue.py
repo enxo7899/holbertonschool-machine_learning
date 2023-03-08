eb4e6b6edfbbf1e8ebbeb0a286b8699c1e0e58d6
enxo7899 <enxo7899@gmail.com>
2023-03-08 05:29:02 -0800

    task 16

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""taak 14"""
+
+
 import numpy as np
+
+
 def np_cat(mat1, mat2, axis=0):
+    """function to calculate mat"""
     return np.concatenate((mat1, mat2), axis=axis)
