803a8aec30730709dbdaf416b5742045804dfd6d
enxo7899 <enxo7899@gmail.com>
2023-03-21 11:01:19 -0700

    task1

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -9,13 +9,13 @@ def convolve_grayscale_same(images, kernel):
     """
 
 
-m, h, w = images.shape
-kh, kw = kernel.shape
-ph = max(kh - 1, 0)
-pw = max(kw - 1, 0)
-padded_images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),                         mode='constant', constant_values=0)
-output = np.zeros((m, h, w))
-for i in range(h):
-    for j in range(w):
-        output[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel,                              axis=(1,2))
-return output
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    ph = max(kh - 1, 0)
+    pw = max(kw - 1, 0)
+    padded_images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),                         mode='constant', constant_values=0)
+    output = np.zeros((m, h, w))
+    for i in range(h):
+        for j in range(w):
+            output[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel,                              axis=(1,2))
+    return output
