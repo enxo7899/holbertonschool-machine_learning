698b3fa1ef04562c9241e724a04b0a21a1487bc3
enxo7899 <enxo7899@gmail.com>
2023-03-21 12:37:39 -0700

    task 4

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -10,20 +10,20 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     Function for RBG images
     """
     m, h, w, c = images.shape
-    kh, kw, kc = kernel.shape
+    kh, kw, _ = kernel.shape
     sh, sw = stride
-    if padding == 'same':
-        ph = int(((h - 1) * sh + kh - h) / 2) + 1
-        pw = int(((w - 1) * sw + kw - w) / 2) + 1
-    elif padding == 'valid':
-        ph = pw = 0
+    if padding == 'valid':
+        ph, pw = 0, 0
+    elif padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
     else:
         ph, pw = padding
-    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
-    out_h = int((h + 2*ph - kh) / sh) + 1
-    out_w = int((w + 2*pw - kw) / sw) + 1
-    output = np.zeros((m, out_h, out_w))
-    for i in range(out_h):
-        for j in range(out_w):
-            output[:, i, j] = np.sum(padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw] * kernel, axis=(1, 2))
+    images_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    conv_h = (h - kh + 2 * ph) // sh + 1
+    conv_w = (w - kw + 2 * pw) // sw + 1
+    output = np.zeros((m, conv_h, conv_w))
+    for i in range(conv_h):
+        for j in range(conv_w):
+            output[:, i, j] = np.sum(images_padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw] * kernel, axis=(1, 2))
     return output
