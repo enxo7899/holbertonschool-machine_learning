a9105c826963af54db4f900f7bdddcf5ca3ffe39
enxo7899 <enxo7899@gmail.com>
2023-03-26 14:35:54 -0700

    task 5

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -18,3 +18,18 @@ class MultiNormal:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = np.dot(data-self.mean, (data-self.mean).T)/(data.shape[1]-1)
+    def pdf(self, x):
+        """
+        A function for pdf
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        if x.ndim != 2 or x.shape[1] != 1 or x.shape[0] != self.mean.shape[0]:
+            raise ValueError(f"x must have the shape ({self.mean.shape[0]}, 1)")
+        d = self.mean.shape[0]
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        norm_const = 1.0/((2*np.pi)**(d/2)*np.sqrt(det))
+        x_mean = x - self.mean
+        exponent = np.exp(-0.5 * np.dot(np.dot(x_mean.T, inv), x_mean))
+        return norm_const * exponent
