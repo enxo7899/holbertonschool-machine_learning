463f30dce42499fb466c39b15b1553c3407e3449
enxo7899 <enxo7899@gmail.com>
2023-03-26 14:46:01 -0700

    task 5

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -22,14 +22,25 @@ class MultiNormal:
         """
         A function for pdf
         """
-        if not isinstance(x, np.ndarray):
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[1] < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        X = data - self.mean
+        self.cov = np.matmul(X, X.T) / (data.shape[1] - 1)
+    def pdf(self, x):
+        """
+        Calculates the PDF at a data point
+        """
+        if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
-        if x.ndim != 2 or x.shape[1] != 1 or x.shape[0] != self.mean.shape[0]:
-            raise ValueError(f"x must have the shape ({self.mean.shape[0]}, 1)")
         d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
         det = np.linalg.det(self.cov)
         inv = np.linalg.inv(self.cov)
-        norm_const = 1.0/((2*np.pi)**(d/2)*np.sqrt(det))
-        x_mean = x - self.mean
-        exponent = np.exp(-0.5 * np.dot(np.dot(x_mean.T, inv), x_mean))
-        return norm_const * exponent
+        norm_const = 1.0 / (np.power((2 * np.pi), float(d) / 2) * np.power(det, 1.0 / 2))
+        x_mu = x - self.mean
+        result = np.exp(-0.5 * np.matmul(np.matmul(x_mu.T, inv), x_mu))
+        return norm_const * result.item()
