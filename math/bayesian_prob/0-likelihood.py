50adafd184773ee894f4b7973484f1ffdc5bd20b
enxo7899 <enxo7899@gmail.com>
2023-04-01 07:28:43 -0700

    Task 1

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -8,16 +8,19 @@ def likelihood(x, n, P):
     """
     A function to calculate likehood
     """
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    if not np.all((P >= 0) & (P <= 1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
-    if not isinstance(n, int) or n <= 0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    binom_coeff = np.math.comb(n, x)
-    likelihoods = binom_coeff * np.power(P, x) * np.power(1 - P, n - x)
-    return likelihoods
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    for value in P:
+        if value > 1 or value < 0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+    factorial = np.math.factorial
+    fact_coefficient = factorial(n) / (factorial(n - x) * factorial(x))
+    likelihood = fact_coefficient * (P ** x) * ((1 - P) ** (n - x))
+    return likelihood
