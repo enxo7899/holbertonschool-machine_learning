d9d011b509d0f67581036c6b14874b7db976aa2b
enxo7899 <enxo7899@gmail.com>
2023-04-01 07:48:22 -0700

    tawsk 1

--- math/bayesian_prob/3-posterior.py
+++ math/bayesian_prob/3-posterior.py
@@ -2,9 +2,6 @@
 """ Performs a function that calculates the posterior propability"""
 
 
-import nunmpy as np
-
-
 def posterior(x, n, P, Pr):
     """
     Calculates the posterior probability for the
