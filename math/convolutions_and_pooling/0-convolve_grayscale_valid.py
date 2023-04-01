f7c460dcd6b6ce695a7b1874118af09b1f43f0e5
enxo7899 <enxo7899@gmail.com>
2023-04-01 06:47:22 -0700

    task 1

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,10 +9,18 @@ def convolve_grayscale_valid(images, kernel):
     """function for valid convolution on grayscale images"""
     m, height, width = images.shape
     kh, kw = kernel.shape
-    convoluted = np.zeros((m, height - kh + 1, width - kw + 1))
-    for h in range(height - kh + 1):
-        for w in range(width - kw + 1):
-            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
-                            axis=1).sum(axis=1)
+    if (kh % 2) == 1:
+        ph = (kh - 1) // 2
+    else:
+        ph = kh // 2
+    if (kw % 2) == 1:
+        pw = (kw - 1) // 2
+    else:
+        pw = kw // 2
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant', constant_values=0)
+    convoluted = np.zeros((m, height, width))
+    for h in range(height):
+        for w in range(width):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel, axis=1).sum(axis=1)
             convoluted[:, h, w] = output
     return convoluted
