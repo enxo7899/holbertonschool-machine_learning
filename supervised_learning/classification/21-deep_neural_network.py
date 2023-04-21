cf3b7280ad4c717bb70e3e2f3a8211249e59b0cb
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-21 15:58:41 +0200

    Update 21-deep_neural_network.py

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -12,6 +12,9 @@ class DeepNeuralNetwork:
     """
 
     def __init__(self, nx, layers):
+        """
+        init function
+        """
 
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -49,6 +52,9 @@ class DeepNeuralNetwork:
         return (self.__weights)
     
     def forward_prop(self, X):
+        """
+        forward function
+        """
         
         self.__cache["A0"] = X
         
@@ -64,18 +70,27 @@ class DeepNeuralNetwork:
         return (A, self.cache)
     
     def cost(self, Y, A):
+        """
+        cost function
+        """
         m = Y.shape[1]
         m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
         cost = (1 / m) * (-(m_loss))
         return (cost)
     
     def evaluate(self, X, Y):
+        """
+        evaluate function
+        """
         A, cache = self.forward_prop(X)
         cost = self.cost(Y, A)
         prediction = np.where(A >= 0.5, 1, 0)
         return (prediction, cost)
     
     def gradient_descent(self, Y, cache, alpha=0.05):
+        """
+        gradient function
+        """
         
         m = Y.shape[1]
         back = {}
