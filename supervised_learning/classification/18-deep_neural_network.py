e0199c896942a47ef16ef407a87758e93cd7ad99
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-21 15:40:34 +0200

    Update 18-deep_neural_network.py

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -22,7 +22,7 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
- 
+
         weights = {}
         previous = nx
 
@@ -30,15 +30,17 @@ class DeepNeuralNetwork:
 
             if type(layer) is not int or layer < 0:
                 raise TypeError("layers must be a list of positive integers")
- 
+
             weights["b{}".format(index)] = np.zeros((layer, 1))
-            weights["W{}".format(index)] = (np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            weights["W{}".format(index)] = (
+                (np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            )
             previous = layer
-     
+
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = weights
- 
+
     @property
     def L(self):
         return (self.__L)
