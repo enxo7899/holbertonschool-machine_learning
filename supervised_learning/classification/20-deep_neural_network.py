a3bf23c4b8a6880188c8eb268741f988be4dcfea
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-21 15:48:20 +0200

    Update 20-deep_neural_network.py

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -70,6 +70,9 @@ class DeepNeuralNetwork:
         return (cost)
     
     def evaluate(self, X, Y):
+        """
+        evaluate function
+        """
         A, cache = self.forward_prop(X)
         cost = self.cost(Y, A)
         prediction = np.where(A >= 0.5, 1, 0)
