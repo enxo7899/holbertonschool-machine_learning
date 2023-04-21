58f99ff0440f192b72aa026cfbece24d511ee4c9
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-21 15:44:45 +0200

    Update 19-deep_neural_network.py

--- supervised_learning/classification/19-deep_neural_network.py
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -75,7 +75,7 @@ class DeepNeuralNetwork:
         """
         function for cost
         """
-        
+
         m = Y.shape[1]
         m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
         cost = (1 / m) * (-(m_loss))
