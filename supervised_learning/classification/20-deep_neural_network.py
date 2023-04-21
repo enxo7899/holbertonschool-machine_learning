58a0578052ffc8ea876751de53d913a8e3d97055
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-21 15:50:40 +0200

    Update 20-deep_neural_network.py

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -22,23 +22,25 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
-            
+
         weights = {}
         previous = nx
-        
+
         for index, layer in enumerate(layers, 1):
-            
+
             if type(layer) is not int or layer < 0:
                 raise TypeError("layers must be a list of positive integers")
-            
+
             weights["b{}".format(index)] = np.zeros((layer, 1))
-            weights["W{}".format(index)] = (np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            weights["W{}".format(index)] = (
+                (np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            )
             previous = layer
-            
+    
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = weights
-        
+
     @property
     def L(self):
         return (self.__L)
@@ -50,25 +52,25 @@ class DeepNeuralNetwork:
     @property
     def weights(self):
         return (self.__weights)
-    
+
     def forward_prop(self, X):
         """
         forward function
         """
-        
+
         self.__cache["A0"] = X
-        
+
         for index in range(self.L):
             W = self.weights["W{}".format(index + 1)]
             b = self.weights["b{}".format(index + 1)]
-            
+
             z = np.matmul(W, self.cache["A{}".format(index)]) + b
             A = 1 / (1 + (np.exp(-z)))
-            
+
             self.__cache["A{}".format(index + 1)] = A
-            
+
         return (A, self.cache)
-    
+
     def cost(self, Y, A):
         """
         cost function
@@ -77,7 +79,7 @@ class DeepNeuralNetwork:
         m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
         cost = (1 / m) * (-(m_loss))
         return (cost)
-    
+
     def evaluate(self, X, Y):
         """
         evaluate function
