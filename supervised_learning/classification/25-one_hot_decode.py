72916af3c7f086369b0a55956176e0ba92ad7ceb
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-23 23:38:27 +0200

    Update 25-one_hot_decode.py

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 def one_hot_decode(one_hot):
+    """
+    function one hot
+    """
     if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
         return None
     decoded = np.argmax(one_hot, axis=0)
