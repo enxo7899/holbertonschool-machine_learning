c2f7b4809095347ee9babb9ac55fa14565685d4c
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-23 23:28:09 +0200

    Update 24-one_hot_encode.py

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -16,7 +16,8 @@ def one_hot_encode(Y, classes):
     Returns:
     a one-hot encoding of Y with shape (classes, m), or None on failure
     """
-    if not isinstance(Y, np.ndarray) or len(Y) < 1 or not isinstance(classes, int) or classes < 1:
+    if not isinstance(Y, np.ndarray)
+    or len(Y) < 1 or not isinstance(classes, int) or classes < 1:
         return None
 
     m = Y.shape[0]
