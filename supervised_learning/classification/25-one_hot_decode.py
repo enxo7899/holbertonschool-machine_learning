cad132dfc6e2dc8e44dda9ec96c9718833e05105
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-23 23:37:00 +0200

    Create 25-one_hot_decode.py

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Perform binary classification with one hidden layer"""
+
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
+        return None
+    decoded = np.argmax(one_hot, axis=0)
+    return decoded
