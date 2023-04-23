898c684e85d9952686ade14208ee93cd028a9805
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-04-23 23:59:07 +0200

    Update 26-deep_neural_network.py

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -3,8 +3,6 @@
 
 
 import numpy as np
-
-
 import pickle
 
 
@@ -15,6 +13,7 @@ class DeepNeuralNetwork:
     """
 
     def __init__(self, nx, layers):
+
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -38,24 +37,68 @@ class DeepNeuralNetwork:
         self.__cache = {}
         self.__weights = weights
 
-    def save(self, filename):
-        """
-        Saves the instance object to a file in pickle format.
-        """
-        if not filename.endswith(".pkl"):
-            filename += ".pkl"
+    @property
+    def L(self):
+        return (self.__L)
 
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
+    @property
+    def cache(self):
+        return (self.__cache)
 
-    @staticmethod
-    def load(filename):
-        """
-        Loads a pickled DeepNeuralNetwork object.
-        """
-        try:
-            with open(filename, "rb") as f:
-                obj = pickle.load(f)
-            return obj
-        except FileNotFoundError:
-            return None
+    @property
+    def weights(self):
+        return (self.__weights)
+
+    def forward_prop(self, X):
+
+        self.__cache["A0"] = X
+
+        for index in range(self.L):
+            W = self.weights["W{}".format(index + 1)]
+            b = self.weights["b{}".format(index + 1)]
+
+            z = np.matmul(W, self.cache["A{}".format(index)]) + b
+            A = 1 / (1 + (np.exp(-z)))
+
+            self.__cache["A{}".format(index + 1)] = A
+
+        return (A, self.cache)
+
+    def cost(self, Y, A):
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
+
+    def evaluate(self, X, Y):
+        A, cache = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return (prediction, cost)
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+
+        m = Y.shape[1]
+        back = {}
+
+        for index in range(self.L, 0, -1):
+
+            A = cache["A{}".format(index - 1)]
+
+            if index == self.L:
+                back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
+            else:
+                dz_prev = back["dz{}".format(index + 1)]
+                A_current = cache["A{}".format(index)]
+                back["dz{}".format(index)] = (
+                    np.matmul(W_prev.transpose(), dz_prev) *
+                    (A_current * (1 - A_current)))
+
+            dz = back["dz{}".format(index)]
+            A_prev = cache["A{}".format(index - 1)]
+            W = self.weights["W{}".format(index)]
+            db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+            dW = (1 / m) * np.matmul(dz, A_prev.T)
+            W_prev = self.weights["W{}".format(index+1)]
+            self.weights["W{}".format(index)] -= alpha * dW
+            self.weights["b{}".format(index)] -= alpha * db
