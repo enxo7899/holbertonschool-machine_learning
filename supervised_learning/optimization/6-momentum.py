237ac84a88ec831dec8122eda7d990b703be51c7
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-03 23:19:10 +0200

    Create 6-momentum.py

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Perform optimization"""
+
+
+import numpy as np
+
+
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    function to create momentum
+    """
+    optimizer = tf.train.MomentumOptimizer(alpha, momentum=beta1)
+    train_op = optimizer.minimize(loss)
+    return train_op
