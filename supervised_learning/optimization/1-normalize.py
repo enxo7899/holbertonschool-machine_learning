b6f25c03d51c9d2863e99366a2b2aa38ad489d0c
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-03 20:48:42 +0200

    Create 1-normalize.py

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Perform optimization"""
+
+
+import numpy as np
+
+def normalize(X, m, s):
+    """
+    Normalize X by subtracting the mean and dividing by the standard deviation
+    """
+    X_norm = (X - m) / s
+    return X_norm
