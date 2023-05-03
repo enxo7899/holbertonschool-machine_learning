ed48f65b29c93394a7b581e7cfb647365198fccc
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-03 23:32:23 +0200

    Create 10-Adam.py

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Perform optimization"""
+
+
+import numpy as np
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    adam op
+    """
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=alpha, 
+        beta_1=beta1, 
+        beta_2=beta2, 
+        epsilon=epsilon)
+    return optimizer.minimize(loss)
