84cc8b8487243568213c0c31090fa21e9a3bc813
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-11 22:56:39 +0200

    Update 3-specificity.py

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -7,7 +7,10 @@ import numpy as np
 
 def specificity(confusion):
     """Calculate the true negative rate (specificity) for each class"""
-    tn = np.sum(confusion) - np.sum(confusion, axis=0) - np.sum(confusion, axis=1) + np.diag(confusion)
+    tn = (
+        np.sum(confusion) - np.sum(confusion, axis=0) -
+        np.sum(confusion, axis=1) + np.diag(confusion)
+    )
     fp = np.sum(confusion, axis=0) - np.diag(confusion)
     tnr = tn / (tn + fp)
     return tnr
