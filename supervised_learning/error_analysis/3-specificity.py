af62bb84a1f6be2d7cea45e86f23b23053ca49ca
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-11 22:55:42 +0200

    Create 3-specificity.py

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function to create confusion matrix"""
+
+
+import numpy as np
+
+
+def specificity(confusion):
+    """Calculate the true negative rate (specificity) for each class"""
+    tn = np.sum(confusion) - np.sum(confusion, axis=0) - np.sum(confusion, axis=1) + np.diag(confusion)
+    fp = np.sum(confusion, axis=0) - np.diag(confusion)
+    tnr = tn / (tn + fp)
+    return tnr
