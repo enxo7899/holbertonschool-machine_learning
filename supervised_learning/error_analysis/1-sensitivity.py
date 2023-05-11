9b7cd5f9d14474a3e6ca8f011d43d84e4f1f3ab6
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-11 22:52:13 +0200

    Update 1-sensitivity.py

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -7,8 +7,6 @@ import numpy as np
 
 def sensitivity(confusion):
     """Sensitiviy function"""
-    
-
     tp = np.diag(confusion)
     fn = np.sum(confusion, axis=1) - tp
     tpr = tp / (tp + fn)
