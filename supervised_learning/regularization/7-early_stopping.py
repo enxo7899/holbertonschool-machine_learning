b91ca4a6b8dc1debf87ded18bd08119f1d68d98f
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-15 23:15:53 +0200

    Create 7-early_stopping.py

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Function l2"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """early stopping"""
+    if opt_cost - cost > threshold:
+        count = 0
+    else:
+        count += 1
+    if count != patience:
+        return False, count
+    else:
+        return True, count
