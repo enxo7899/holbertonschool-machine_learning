8cb9f2ad51af5c91773db29dd56dc2d0f19ea5d8
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-15 23:01:39 +0200

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -5,18 +5,14 @@
 import numpy as np
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    
+    """Droupout function"""
     outputs = {}
     outputs["A0"] = X
-    
     for index in range(L):
-        
         weight = weights["W{}".format(index + 1)]
         bias = weights["b{}".format(index + 1)]
-        
         z = np.matmul(weight, outputs["A{}".format(index)]) + bias
         dropout = np.random.binomial(1, keep_prob, size=z.shape)
-        
         if index != (L - 1):
             A = np.tanh(z) # apply tahn activation
             A *= dropout   # apply dropout mask
@@ -26,5 +22,4 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             A = np.exp(z)  # apply softmax
             A /= np.sum(A, axis=0, keepdims=True)
         outputs["A{}".format(index + 1)] = A
-        
     return outputs
