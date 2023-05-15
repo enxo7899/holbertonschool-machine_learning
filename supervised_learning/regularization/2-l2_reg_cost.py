5e0ba864a6e63cc63e26f54ccf478941f6eb87bc
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-15 23:24:31 +0200

    Update 2-l2_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -5,7 +5,9 @@
 import tensorflow as tf
 
 
-def l2_reg_cost(cost, lam):
-    """def for t1"""
-    l2_reg_cost = tf.losses.get_regularization_losses()
-    return (cost + l2_reg_cost)
+def l2_reg_cost(cost):
+    """def"""
+    l2_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    l2_cost = tf.reduce_sum(l2_loss)
+    total_cost = cost + l2_cost
+    return total_cost
