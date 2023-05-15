6199a0c152da69e4409d252cc5fd10d3fd3b955e
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-15 23:47:37 +0200

    Update 6-dropout_create_layer.py

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -2,14 +2,12 @@
 """Function l2"""
 
 
-import numpy as np
-
+import tensorflow as tf
 
 def dropout_create_layer(prev, n, activation, keep_prob):
-    """Def dropout"""
-    dropout = tf.layers.Dropout(keep_prob)
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    tensor = tf.layers.Dense(units=n, activation=activation,
-                             kernel_initializer=init,
-                             kernel_regularizer=dropout)
-    return tensor(prev)
+    dropout = tf.layers.Dropout(keep_prob)
+    dense = tf.layers.Dense(units=n, activation=activation, kernel_initializer=init)
+    dropout_layer = dropout(prev)
+    output = dense(dropout_layer)
+    return output
