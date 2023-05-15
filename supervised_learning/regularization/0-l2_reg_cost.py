5c7d77027e3deb9430e8a77ad0b77937985e6d0b
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-15 16:39:03 +0200

    Update 0-l2_reg_cost.py

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -4,7 +4,7 @@ import numpy as np
 def l2_reg_cost(cost, lambtha, weights, L, m):
   l2_cost = 0
   for i in range (1, L+1):
-    l2_cost_ += np.sum(np.square(weights['W+str(i)]))
+    l2_cost += np.sum(np.square(weights['W'+str(i)]))
                                          
   l2_cost *= lambtha/(2*m)
   return cost + l2_cost
