150616df0980ae2b6a1470465a855dbe4db5fc83
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-05-19 22:26:04 +0200

    Update 1-input.py

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -10,13 +10,13 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     reg_l2 = K.regularizers.l2(lambtha)
     inputs = K.Input(shape=(nx,))
     X = inputs
-    
+
     for i in range(len(layers)):
         if i != 0:
             X = K.layers.Dropout(1 - keep_prob)(X)
         X = K.layers.Dense(layers[i],
                            activation=activations[i],
                            kernel_regularizer=reg_l2)(X)
-        
+
     model = K.Model(inputs=inputs, outputs=X)
     return model
