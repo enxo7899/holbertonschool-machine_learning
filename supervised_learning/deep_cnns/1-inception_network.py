a66ba1ec88b4111b9ee8c36113bb3fecc2c778cd
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-06-19 15:58:07 +0200

    Update 1-inception_network.py

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -6,7 +6,7 @@ using Keras model
 
 
 import tensorflow.keras as K
-inception_network = __import__('1-inception_network').inception_network
+inception_network = __import__('0-inception_network').inception_network
 
 
 def inception_network():
