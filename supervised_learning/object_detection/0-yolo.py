5ef2ae343feb253da69d1db18f40718a6fb5d2ed
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-07-01 20:25:33 +0200

    Update 0-yolo.py

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+"""Task 0. Initialize Yolo"""
+from tensorflow.keras.models import load_model
 
+
+class Yolo:
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        self.model = load_model(model_path)
+        with open(classes_path) as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
