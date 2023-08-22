693dc8a74460339e0133008a582af1c68a33125f
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-08-22 14:30:03 +0200

    Create 0-bag_of_words.py

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+def bag_of_words(sentences, vocab=None):
+    if vocab is None:
+        vectorizer = CountVectorizer()
+    else:
+        vectorizer = CountVectorizer(vocabulary=vocab)
+
+    embeddings = vectorizer.fit_transform(sentences).toarray()
+    features = vectorizer.get_feature_names_out()
+
+    return embeddings, features
