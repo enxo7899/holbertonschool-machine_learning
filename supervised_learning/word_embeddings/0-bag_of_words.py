0715e527b414e99c9ad9816200175cf165414e81
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-08-24 16:14:45 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -12,3 +12,16 @@ def bag_of_words(sentences, vocab=None):
     features = vectorizer.get_feature_names_out()
 
     return embeddings, features
+
+if __name__ == "__main__":
+    sentences = ["Holberton school is Awesome!",
+                 "Machine learning is awesome",
+                 "NLP is the future!",
+                 "The children are our future",
+                 "Our children's children are our grandchildren",
+                 "The cake was not very good",
+                 "No one said that the cake was not very good",
+                 "Life is beautiful"]
+    E, F = bag_of_words(sentences)
+    print(E)
+    print(F)
