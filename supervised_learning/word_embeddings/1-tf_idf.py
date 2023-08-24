3634124ac356b6c525fe00f98f2e85b19e0c282f
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-08-24 16:30:37 +0200

    Update 1-tf_idf.py

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
+"""
+Defines a funcition for tf_idf
+"""
+
+
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 def tf_idf(sentences, vocab=None):
+    """
+    function tf_idf
+    """
     if vocab is None:
         vectorizer = TfidfVectorizer()
     else:
         vectorizer = TfidfVectorizer(vocabulary=vocab)
 
     embeddings = vectorizer.fit_transform(sentences).toarray()
-    features = vectorizer.get_feature_names_out()
+    features = vectorizer.get_feature_names()
 
     return embeddings, features
+
+if __name__ == "__main__":
+    sentences = ["Holberton school is Awesome!",
+                 "Machine learning is awesome",
+                 "NLP is the future!",
+                 "The children are our future",
+                 "Our children's children are our grandchildren",
+                 "The cake was not very good",
+                 "No one said that the cake was not very good",
+                 "Life is beautiful"]
+    vocab = ["awesome", "learning", "children", "cake", "good", "none", "machine"]
+    E, F = tf_idf(sentences, vocab)
+    print(E)
+    print(F)
