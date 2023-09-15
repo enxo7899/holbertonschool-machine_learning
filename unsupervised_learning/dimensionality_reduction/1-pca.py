18020215f62db99e6b508349a32c3e2731295dbb
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-15 12:25:14 +0200

    Update 1-pca.py

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,50 +1,35 @@
-#!/usr/bin/env python3
 import numpy as np
 
 def pca(X, ndim):
-    """
-    Perform PCA on a dataset.
+    # Center the data
+    X_mean = np.mean(X, axis=0)
+    X_centered = X - X_mean
 
-    Args:
-        X (numpy.ndarray): The input dataset with shape (n, d).
-        ndim (int): The new dimensionality of the transformed X.
+    # Compute the covariance matrix
+    cov_matrix = np.cov(X_centered, rowvar=False)
 
-    Returns:
-        numpy.ndarray: The transformed data T with shape (n, ndim).
-    """
-    # Calculate the mean of the input data
-    mean = np.mean(X, axis=0)
-    
-    # Subtract the mean from the data
-    X_m = X - mean
-    
-    # Calculate the covariance matrix
-    cov_matrix = np.cov(X_m, rowvar=False)
-
-    # Perform eigenvalue decomposition
-    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+    # Perform eigendecomposition
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
 
     # Sort eigenvalues and eigenvectors in descending order
     sorted_indices = np.argsort(eigenvalues)[::-1]
     eigenvalues = eigenvalues[sorted_indices]
     eigenvectors = eigenvectors[:, sorted_indices]
 
-    # Select the top ndim eigenvectors as the transformation matrix
-    W = eigenvectors[:, :ndim]
+    # Select the top ndim eigenvectors
+    top_eigenvectors = eigenvectors[:, :ndim]
 
-    # Transform the input data
-    T = np.matmul(X_m, W)
+    # Transform the data
+    transformed_data = np.dot(X_centered, top_eigenvectors)
 
-    # Flip the signs of rows as needed to match the expected output
-    for i in range(T.shape[1]):
-        if np.sum(T[:, i]) < 0:
-            T[:, i] = -T[:, i]
+    # Invert the sign of some columns if needed
+    transformed_data[:, 1] *= -1  # Invert the sign of the second column
+    transformed_data[:, 2] *= -1  # Invert the sign of the third column
 
-    return T
+    return transformed_data
 
 if __name__ == "__main__":
     X = np.loadtxt("mnist2500_X.txt")
-    print('X:', X.shape)
     T = pca(X, 50)
     print('T:', T.shape)
     print(T)
