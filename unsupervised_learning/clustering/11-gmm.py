c33b3d4badb1bb56a579e19e5226db74b9820c0c
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-20 19:25:55 +0200

    Update 11-gmm.py

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -8,14 +8,14 @@ def gmm(X, k):
     """
     calculates a GMM from a dataset
     """
-    
+
     g = sklearn.mixture.GaussianMixture(n_components=k)
     g.fit(X)
-    
+
     pi = g.weights_
     m = g.means_
     S = g.covariances_
     clss = g.predict(X)
     bic = g.bic(X)
-    
+
     return pi, m, S, clss, bic
