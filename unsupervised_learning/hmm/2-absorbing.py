fcaaa1c3841133a5c012a9146c0d02e27432b83d
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-28 11:56:45 +0100

    Create 2-absorbing.py

---
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" Absorbing Markov chain"""
+
+import numpy as np
+
+
+def absorbing(P):
+    """
+    Function that determines if a markov chain is absorbing
+    """
+    if len(P.shape) != 2:
+        return None
+
+    n1, n2 = P.shape
+    if (n1 != n2) or type(P) is not np.ndarray:
+        return None
+
+    D = np.diagonal(P) # transition probabilities from each state to itself
+
+    # extreme cases where either all states are absorbing or none of them are
+    if (D == 1).all():
+        return True
+    if not (D == 1).any():
+        return False
+
+    # formula t=(I-Q)^-1 1
+    # approach using Neumman series: t = (∑ inf k=0 * Q^k) * 1
+    # suggested implementation based on: https://stackoverflow.com/
+    # questions/45164505/best-iterative-way-to-calculate-the-fundamental-matrix-
+    # of-an-absorbing-markov-ch
+
+
+    """
+    For each diagonal element (where i == j)
+    check the transition probabilities of the next state  and the previous state
+    If both of these probabilities are zero, it implies that the current state is not absorbing
+    as there are transitions out of it
+    """
+    for i in range(n1):
+        # print('this is Pi {}'.format(P[i]))
+        for j in range(n2):
+            # print('this is Pj {}'.format(P[j]))
+            if (i == j) and (i + 1 < len(P)):
+                if P[i + 1][j] == 0 and P[i][j + 1] == 0:
+                    return False
+    return True
