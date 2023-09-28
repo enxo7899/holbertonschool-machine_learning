7afa5468a468beeaf61c2d1dc9e6cef548c8642f
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-28 13:28:33 +0100

    Update 0-gp.py

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -6,7 +6,11 @@
 
 import numpy as np
 
+
 class GaussianProcess:
+    """
+    Gaussian Process Class
+    """
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         self.X = X_init
         self.Y = Y_init
@@ -14,6 +18,7 @@ class GaussianProcess:
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
 
+    
     def kernel(self, X1, X2):
         m, n = X1.shape[0], X2.shape[0]
         K = np.zeros((m, n))
