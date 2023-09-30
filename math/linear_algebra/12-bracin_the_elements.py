0092448d3976bbc9d88d13c1a4cb9da9dad21ec3
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-30 11:23:29 +0100

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that preforms aritmetic operations to matrices
+"""
 
-import numpy as np
 
 def np_elementwise(mat1, mat2):
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
-    return (add, sub, mul, div)
+    """
+    Needs a matrix as input
+    Returns the resulting matrix
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
