a00edb94b6fae9a00dad9453979f351472873db0
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-30 10:37:37 +0100

    Update 2-absorbing.py

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,47 +1,31 @@
 #!/usr/bin/env python3
-""" Absorbing Markov chain"""
+"""
+Defines function that determines if the Markov Chain is absorbing
+"""
+
 
 import numpy as np
 
 
 def absorbing(P):
     """
-    Function that determines if a markov chain is absorbing
+    Determines if the Markov Chain is absorbing
+
+    parameters:
+        P [square 2D numpy.ndarray of shape (n, n)]:
+            representing the standard transition matrix
+            P[i, j] is the probability of transitioning from state i to state j
+            n: the number of state in the Markov Chain
+
+    returns:
+        True, if absorbing
+        False, if not absorbing or on failure
     """
-    if len(P.shape) != 2:
-        return None
-
-    n1, n2 = P.shape
-    if (n1 != n2) or type(P) is not np.ndarray:
-        return None
-
-    
-    D = np.diagonal(P) # transition probabilities from each state to itself
-
-
-    # extreme cases where either all states are absorbing or none of them are
-    if (D == 1).all():
-        return True
-    if not (D == 1).any():
+    # check that P is the correct type and dimensions
+    if type(P) is not np.ndarray or len(P.shape) != 2:
         return False
-
-    # formula t=(I-Q)^-1 1
-    # approach using Neumman series: t = (∑ inf k=0 * Q^k) * 1
-    # suggested implementation based on: https://stackoverflow.com/
-    # questions/45164505/best-iterative-way-to-calculate-the-fundam
-    # of-an-absorbing-markov-ch
-
-    """
-    For each diagonal element (where i == j)
-    check the transition probabilities of the next state  and the previous
-    If both of these probabilities are zero, it implies that the current
-    as there are transitions out of it
-    """
-    for i in range(n1):
-        # print('this is Pi {}'.format(P[i]))
-        for j in range(n2):
-            # print('this is Pj {}'.format(P[j]))
-            if (i == j) and (i + 1 < len(P)):
-                if P[i + 1][j] == 0 and P[i][j + 1] == 0:
-                    return False
+    # save value of n and check that P is square
+    n, n_check = P.shape
+    if n != n_check:
+        return False
     return True
