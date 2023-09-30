9c7fb7ba2b04d8618e9fc133fa64b56f8cfeddb2
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-30 11:18:42 +0100

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
-
-import numpy as np
+"""
+Creates a bag of words embedding matrix
+"""
 from sklearn.feature_extraction.text import CountVectorizer
 
-def bag_of_words(sentences, vocab=None):
-    if vocab is None:
-        vectorizer = CountVectorizer()
-    else:
-        vectorizer = CountVectorizer(vocabulary=vocab)
 
-    embeddings = vectorizer.fit_transform(sentences).toarray()
-    features = vectorizer.get_feature_names_out()
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag of words embedding matrix
+    :param sentences: a list of sentences to analyze
+    :param vocab: a list of the vocabulary words to use for the analysis
+    :return: embeddings, features
+    embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
+        s is the number of sentences in sentences
+        f is the number of features analyzed
+    features is a list of the features used for embeddings
+    """
+    vector = CountVectorizer(vocabulary=vocab)
+    X = vector.fit_transform(sentences)
+    features = vector.get_feature_names()
+    embeddings = X.toarray()
 
     return embeddings, features
-
-if __name__ == "__main__":
-    sentences = ["Holberton school is Awesome!",
-                 "Machine learning is awesome",
-                 "NLP is the future!",
-                 "The children are our future",
-                 "Our children's children are our grandchildren",
-                 "The cake was not very good",
-                 "No one said that the cake was not very good",
-                 "Life is beautiful"]
-
-    E, F = bag_of_words(sentences)
-    print("Vocab is None:")
-    print(E)
-    print(F)
-    
-    custom_vocab = ['awesome', 'future', 'cake', 'good', 'is', 'learning', 'beautiful']
-    E, F = bag_of_words(sentences, vocab=custom_vocab)
-    print("\nVocab is not None:")
-    print(E)
-    print(F)
