fb44c4082c2964ba0cfe02087d4480dd275ac2a8
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-30 11:09:14 +0100

    Update 6-expectation.py

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,46 +1,52 @@
 #!/usr/bin/env python3
-""" E-step: expected value of likelihood function
-    respect the conditional distribution
 """
-
+Calculates the expectation step in the EM algorithm for a GMM
+"""
 import numpy as np
+pdf = __import__('5-pdf').pdf
+
 
 def expectation(X, pi, m, S):
     """
-    Function that calculates the expectation step in the EM algorithm for a GMM
+    Calculates the expectation step in the EM algorithm for a GMM
+    :param X: numpy.ndarray of shape (n, d) containing the data set
+    :param pi: numpy.ndarray of shape (k,) containing the priors for each
+    cluster
+    :param m: numpy.ndarray of shape (k, d) containing the centroid means for
+    each cluster
+    :param S: numpy.ndarray of shape (k, d, d) containing the covariance
+    matrices for each cluster
+    :return: g, l, or None, None on failure
+        g is a numpy.ndarray of shape (k, n) containing the posterior
+        probabilities for each data point in each cluster
+        l is the total log likelihood
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
-    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+    if type(pi) is not np.ndarray or len(pi.shape) != 1:
         return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+    if type(m) is not np.ndarray or len(m.shape) != 2:
         return None, None
-    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+    if type(S) is not np.ndarray or len(S.shape) != 3:
         return None, None
-    if not np.isclose([np.sum(pi)], [1])[0]:
-        return None, None
-
-    n, d = X.shape
     k = pi.shape[0]
+    n, d = X.shape
+
+    if k > n:
+        return None, None
     if d != m.shape[1] or d != S.shape[1] or d != S.shape[2]:
         return None, None
     if k != m.shape[0] or k != S.shape[0]:
         return None, None
+    if not np.isclose([np.sum(pi)], [1])[0]:
+        return None, None
 
-    centroids_mean = m
-    covariance_mat = S
-    gauss_components = np.zeros((k, n))
-
-    # http://ethen8181.github.io/machine-learning/clustering/GMM/GMM.html
-    # P(j) P(Xn | j) / ∑k j=1 P(j) P(Xn | j)
+    probs = np.zeros((k, n))
     for i in range(k):
-        likelihood = pdf(X, centroids_mean[i], covariance_mat[i])
-        prior = pi[i]
-        gauss_components[i] = likelihood * prior
-    g = gauss_components / np.sum(gauss_components, axis=0)
+        probs[i] = pi[i] * pdf(X, m[i], S[i])
 
-    # https://zhiyzuo.github.io/EM/
-    # Log likelihood: ∑i ln ∑z^i p((x^i) , (z^i);Θ)
-    log_likelihood = np.sum(np.log(np.sum(gauss_components, axis=0)))
+    marginal = np.sum(probs, axis=0)
+    g = probs / marginal
+    log_likelihood = np.sum(np.log(marginal))
 
     return g, log_likelihood
