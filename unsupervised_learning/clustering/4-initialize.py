56d97552810dc7a29ccd48d89604fd0d705b3ea6
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-30 11:08:30 +0100

    Update 4-initialize.py

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
-"""GMM function """
-
+"""
+Initializes variables for a Gaussian Mixture Model
+"""
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
     """
-    initializes variables for a Gaussian Mixture Model
+    Initializes variables for a Gaussian Mixture Model
+    :param X: numpy.ndarray of shape (n, d) containing the data set
+    :param k: positive integer containing the number of clusters
+    :return: pi, m, S, or None, None, None on failure
+        pi is a numpy.ndarray of shape (k,) containing the priors for each
+        cluster, initialized evenly
+        m is a numpy.ndarray of shape (k, d) containing the centroid means for
+        each cluster, initialized with K-means
+        S is a numpy.ndarray of shape (k, d, d) containing the covariance
+        matrices for each cluster, initialized as identity matrices
     """
-
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k < 1:
+    if type(k) is not int or k <= 0:
         return None, None, None
 
     n, d = X.shape
+    centroids, clss = kmeans(X, k, iterations=1000)
+    pi = np.ones(k) / k
+    m = centroids
+    S = np.tile(np.identity(d), (k, 1)).reshape((k, d, d))
 
-    # priors for each cluster, initialized evenly
-    phi = np.ones(k)/k
-
-    # centroid means for each cluster, initialized with K-means
-    m, _ = kmeans(X, k)
-
-    # covariance matrices for each cluster, initialized as identity matrices
-    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
-
-    return phi, m, S
+    return pi, m, S
