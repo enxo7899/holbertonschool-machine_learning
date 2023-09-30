1800011316211c224540544d90e338514d21cd42
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-30 10:52:48 +0100

    Update 1-pca.py

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
-
+"""
+0-pca.py
+"""
 import numpy as np
 
+
 def pca(X, ndim):
-    # Center the data
-    X_mean = np.mean(X, axis=0)
-    X_centered = X - X_mean
+    """function that performs principal components analysis on a dataset"""
 
-    # Compute the covariance matrix
-    cov_matrix = np.cov(X_centered, rowvar=False)
+    # Useful resource: https://www.youtube.com/watch?v=NUn6WeFM5cM
 
-    # Perform eigendecomposition
-    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+    # PCA defined as the eigendecomposition of the covariance matrix dot(X.T,X)
+    # -> returns a set of eigenvectors (W:loadings) and eigenvalues (lambda)
+    # Each column of W is a principal component; columns ordered by how large
+    # their corresponding eigenvalues are; i.e. principal component
+    # corresponding to the largest eigenvalue is always going to be the first
+    # one, Etc.; r: number of principal components to extract -> truncate W
+    # -> extract the first r columns of W
+    # T:scores, defined as T = dot(X,W) -> Tr = dot(X,Wr)
+    # -> Tr new representation of X (in the W space)
 
-    # Sort eigenvalues and eigenvectors in descending order
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[sorted_indices]
-    eigenvectors = eigenvectors[:, sorted_indices]
+    # Better way of computing W: Singular Value Decomposition (SVD)
+    # X = dot(U,S,V.T); U: right singular vector, V:left singular vector
+    # S: singular values on the diagonal (proportional to eigenvalues; ordered)
+    # Importantly (it can be demonstrated that): V = W
+    # T = dot(X,W) && X = dot(U,S,V.T) -> dot(X,V) = dot(U,S,V.T,V)
+    # since dot(U.T,U) = I and dot(V.T,V) = I -> dot(X,V) = dot(U,S)
+    # since V = W -> dot(X,W) = dot(U,S) -> T = dot(U,S) -> Tr = dot(Ur,Sr)
 
-    # Select the top ndim eigenvectors
-    top_eigenvectors = eigenvectors[:, :ndim]
+    # Ensure that all dimensions have a mean of 0 across all data points
+    X = X - np.mean(X, axis=0)
 
-    # Transform the data
-    transformed_data = np.dot(X_centered, top_eigenvectors)
+    # Compute the SVD:
+    U, S, Vt = np.linalg.svd(X)
+    # print(S)
 
-    # Invert the sign of some columns if needed
-    transformed_data[:, 1] *= -1  # Invert the sign of the second column
-    transformed_data[:, 2] *= -1  # Invert the sign of the third column
+    # Compute Tr, the transformed matrix X:
+    # print(U[..., :ndim])
+    # print(np.diag(S[..., :ndim]))
+    Tr = np.matmul(U[..., :ndim], np.diag(S[..., :ndim]))
 
-    return transformed_data
-
-if __name__ == "__main__":
-    X = np.loadtxt("mnist2500_X.txt")
-    T = pca(X, 50)
-    T[:, 2] *= -1  # Invert the sign of the third column
-    print('T:', T.shape)
-    print(T)
+    return Tr
