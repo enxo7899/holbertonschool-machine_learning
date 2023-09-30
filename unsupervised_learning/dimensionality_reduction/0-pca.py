558e9e821c44a3879979d1711a985ae7e4bb42c8
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-09-30 10:52:11 +0100

    Update 0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,58 +1,53 @@
 #!/usr/bin/env python3
+"""
+0-pca.py
+"""
 import numpy as np
 
+
 def pca(X, var=0.95):
-    """
-    Perform PCA on a dataset.
+    """function that performs principal components analysis on a dataset"""
 
-    Args:
-        X (numpy.ndarray): The input dataset with shape (n, d).
-        var (float): The fraction of variance to maintain (default is 0.95).
+    # Useful resource: https://www.youtube.com/watch?v=NUn6WeFM5cM
 
-    Returns:
-        numpy.ndarray: The transformed data T with shape (n, nd) where nd is
-                      the new dimensionality of the transformed X.
-        numpy.ndarray: The weights matrix W with shape (d, nd).
-    """
-    # Calculate the covariance matrix
-    cov_matrix = np.cov(X, rowvar=False)
+    # PCA defined as the eigendecomposition of the covariance matrix dot(X.T,X)
+    # -> returns a set of eigenvectors (W:loadings) and eigenvalues (lambda)
+    # Each column of W is a principal component; columns ordered by how large
+    # their corresponding eigenvalues are; i.e. principal component
+    # corresponding to the largest eigenvalue is always going to be the first
+    # one, Etc.; r: number of principal components to extract -> truncate W
+    # -> extract the first r columns of W
+    # T:scores, defined as T = dot(X,W) -> Tr = dot(X,Wr)
+    # -> Tr new representation of X (in the W space)
 
-    # Perform eigenvalue decomposition
-    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+    # Better way of computing W: Singular Value Decomposition (SVD)
+    # X = dot(U,S,V.T); U: right singular vector, V:left singular vector
+    # S: singular values on the diagonal (proportional to eigenvalues; ordered)
+    # Importantly (it can be demonstrated that): V = W
+    # T = dot(X,W) && X = dot(U,S,V.T) -> dot(X,V) = dot(U,S,V.T,V)
+    # since dot(U.T,U) = I and dot(V.T,V) = I -> dot(X,V) = dot(U,S)
+    # since V = W -> dot(X,W) = dot(U,S) -> T = dot(U,S) -> Tr = dot(Ur,Sr)
 
-    # Sort eigenvalues and eigenvectors in descending order
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[sorted_indices]
-    eigenvectors = eigenvectors[:, sorted_indices]
+    # Compute the SVD:
+    U, S, Vt = np.linalg.svd(X)
+    # print(S)
 
-    # Calculate the cumulative explained variance
-    explained_variance_ratio = np.cumsum(eigenvalues) / np.sum(eigenvalues)
+    # Compute the commulated sum of the singular (s) values from S:
+    sum_s = np.cumsum(S)
+    # print(sum_s)
+    # sum_s is a 1D array of this type: array([ 1,  3,  6, 10, 15, 21])
 
-    # Determine the number of dimensions to keep
-    nd = np.argmax(explained_variance_ratio >= var) + 1
+    # Infer 'r' (number of principal components to extract from W/V)
+    # based on the 'var' treshold passed as argument to the method
+    # Normalize sum_s:
+    sum_s = sum_s / sum_s[-1]
+    # print(sum_s)
+    # Note: here np.where() returns an array of indices
+    r = np.min(np.where(sum_s >= var))
+    # print(r)
 
-    # Select the top nd eigenvectors as the transformation matrix
-    W = eigenvectors[:, :nd]
+    # Compute Vr(= Wr):
+    V = Vt.T
+    Vr = V[..., :r + 1]
 
-    # Transform the input data
-    T = np.matmul(X, W)
-
-    return T, W
-
-if __name__ == "__main__":
-    np.random.seed(0)
-    a = np.random.normal(size=50)
-    b = np.random.normal(size=50)
-    c = np.random.normal(size=50)
-    d = 2 * a
-    e = -5 * b
-    f = 10 * c
-
-    X = np.array([a, b, c, d, e, f]).T
-    m = X.shape[0]
-    X_m = X - np.mean(X, axis=0)
-    T, W = pca(X_m)
-    print(T)
-    print(T.shape)
-    print(W)
-    print(W.shape)
+    return Vr
