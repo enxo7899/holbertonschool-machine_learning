300ca3b37eed19100934de6539be4895d21f519f
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-10-25 11:21:56 +0100

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python3
 """
-Uses the GitHub API to print the location of a specific user,
-where user is passed as first argument of the script with full API URL
-
-ex) "./2-user_location.py https://api.github.com/users/holbertonschool"
+Prints the location of a specific Github user
 """
-
-
 import requests
-from sys import argv
-from time import time
+import sys
+import time
 
 
-if __name__ == "__main__":
-    if len(argv) < 2:
-        raise TypeError(
-            "Input must have the full API URL passed in as an argument: {}{}".
-            format('ex. "./2-user_location.py',
-                   'https://api.github.com/users/holbertonschool"'))
-    try:
-        url = argv[1]
-        results = requests.get(url)
-        if results.status_code == 403:
-            reset = results.headers.get('X-Ratelimit-Reset')
-            waitTime = int(reset) - time()
-            minutes = round(waitTime / 60)
-            print('Reset in {} min'.format(minutes))
-        else:
-            results = results.json()
-            location = results.get('location')
-            if location:
-                print(location)
-            else:
-                print('Not found')
-    except Exception as err:
-        print('Not found')
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        exit()
+    url = sys.argv[1]
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        print(r.json()['location'])
+
+    if r.status_code == 404:
+        print("Not found")
+
+    if r.status_code == 403:
+        rate_limit = int(r.headers['X-Ratelimit-Reset'])
+        now = int(time.time())
+        minutes = int((rate_limit - now) / 60)
+        print("Reset in {} min".format(minutes))
