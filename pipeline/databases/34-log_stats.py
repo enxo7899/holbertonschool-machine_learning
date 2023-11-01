5fbeda23a1882c9057e5d42706ebe359d9349f31
enxo7899 <61548627+enxo7899@users.noreply.github.com>
2023-11-01 11:31:30 +0000

    Update 34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
-"""
-This script provides some stats about Nginx logs stored in MongoDB
-"""
+"""Provides stats about Nginx logs stored in MongoDB."""
+
 
 from pymongo import MongoClient
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection_logs = client.logs.nginx
-    num_docs = collection_logs.count_documents({})
-    print("{} logs".format(num_docs))
+    logs = client.logs.nginx
+    doc_count = logs.count_documents({})
+    print("{} logs".format(doc_count))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        num_met = collection_logs.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, num_met))
-
-    my_dicti = {"method": "GET", "path": "/status"}
-
-    num_dicti = collection_logs.count_documents(my_dicti)
-    print("{} status check".format(num_dicti))
+    for m in methods:
+        cnt = logs.count_documents({"method": m})
+        print("\tmethod {}: {}".format(m, cnt))
+    ftp = {"method": "GET", "path": "/status"}
+    cnt = logs.count_documents(ftp)
+    print("{} status check".format(cnt))
